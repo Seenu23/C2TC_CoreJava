61cf17e321dade8cc1b79d06182a70473758835e
srini <srini@SEENULAPTOP-2DOFRJ58>
2022-08-26 07:09:05 +0530

    commit

--- JavaDemos/src/com/tnsif/lesson3e/Five.java
+++ JavaDemos/src/com/tnsif/lesson3e/Five.java
@@ -1,10 +1,18 @@
 package com.tnsif.lesson3e;
-
-public class Five {
-
+import com.tnsif.lesson3f.*;
+class Five {
+	public void dispOutput()
+	{
+		System.out.println("I am Five");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
+		Six obj = new Six();
+		obj.dispOutput();
+		
+		
 	}
 
 }
